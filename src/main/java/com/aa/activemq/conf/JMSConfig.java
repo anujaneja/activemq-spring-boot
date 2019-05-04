3c9b130155225471a78a40a989d8d86be567c3c0
Anuj Aneja <anuj.aneja@unicommerce.com>
2019-05-04 11:44:12 +0530

    optimised imports

--- src/main/java/com/aa/activemq/conf/JMSConfig.java
+++ src/main/java/com/aa/activemq/conf/JMSConfig.java
@@ -1,15 +1,11 @@
 package com.aa.activemq.conf;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.command.ActiveMQQueue;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
-
-import javax.jms.Queue;
 
 @Configuration
 @EnableJms
